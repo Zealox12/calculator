456b5d6909cfa13faa3f196aae2a36e403c7798f
Zealox <hk08694@st.habib.edu.pk>
2025-07-06 17:53:29 +0500

    Do debugging and complete the project
    Debugged syntax and logical errors, did some CSS styling to make the calculator
    look better, completed the project

--- javascript.js
+++ javascript.js
@@ -14,9 +14,6 @@ function divide(a,b){
     return a/b;
 }
 
-let num1 = 0;
-let num2 = 0;
-let operator = "";
 
 function operate(operator,num1,num2){
     switch (operator){
@@ -33,7 +30,117 @@ function operate(operator,num1,num2){
             return divide(num1,num2);
             break;
         default:
-            alert("Wrong Operator Entered");
+            return NaN;
 
     }
 }
+
+const display = document.querySelector(".display")
+const buttons = document.querySelector(".buttons")
+
+let result = 0; //result of our calculations
+
+const operationsText = "+-*/";
+const numbers = "0123456789.";
+
+
+let num1 = null;
+let num2 = null;
+let operator = "";
+
+let resultFlag = false;
+
+function clearCalculator(){
+    num1 = null;
+    num2 = null;
+    operator = "";
+    display.textContent = "";
+    resultFlag = false;
+}
+
+function displayUpdate(item){
+    // if(item === "clear")
+    // {
+    //     display.textContent = "";
+    // }
+    if(numbers.includes(item)){
+        display.textContent += item;
+    }
+    else if(operationsText.includes(item)){
+        if (display.textContent && 
+        !operationsText.includes(display.textContent.slice(-1))){
+            display.textContent += ` ${item} `;
+        }
+    }
+}
+
+buttons.addEventListener("click",(e)=>
+{
+    const input = e.target.textContent;
+    if(input === "clear"){
+        clearCalculator();
+        return;
+    }
+    if (resultFlag && numbers.includes(input)) {
+        display.textContent = "";
+        resultFlag = false;
+    }
+    
+    if (numbers.includes(input)) {
+        displayUpdate(input);
+    }
+    else if(input === "="){
+        if(num1 === null || operator === "")
+        {
+            alert("Invalid operation");
+            clearCalculator();
+            return;
+        }
+        num2 = parseFloat(display.textContent.slice(
+            display.textContent.indexOf(operator)+2));
+        if (isNaN(num2)) {
+            alert("Invalid number");
+            clearCalculator();
+            return;
+        }
+        if(num2 === 0 && operator === "/"){
+            alert("Bro Whatch yo doin??");
+            clearCalculator();
+            return;
+        }
+        result = operate(operator,num1,num2);
+        result = Math.round(result * 1000) / 1000;
+        display.textContent = result.toString();
+        num1 = result;
+        resultFlag = true;
+        operator = "";
+    }
+    else if(operationsText.includes(input)){
+        if (resultFlag){
+            resultFlag = false;
+        }
+        if(operator && num1 !== null){
+            const temp = display.textContent.split(" ");
+            if (temp.length >=3){
+                num2 = parseFloat(temp[2]);
+                if(!isNaN(num2)){
+                    result = operate(operator,num1,num2);
+                    result = Math.round(result * 1000) / 1000;
+                    display.textContent = result.toString() + ` ${input} `;
+                    num1 = result;
+                    operator = input;
+                    return;
+                }
+            }
+        }
+        if (operator === ""){
+            num1 = parseFloat(display.textContent);
+            operator = input;
+            displayUpdate(input);
+        }
+        else {
+            display.textContent = display.textContent.slice(0, -3) + ` ${input} `;
+            operator = input;
+        }
+    }
+})
